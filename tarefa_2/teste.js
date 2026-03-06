9bba06fb5805929158e1eb8ff99a2264cf13ad42
João Antônio <tontjoao@gmail.com>
2026-03-06 16:28:39 -0300

    Refactor variable declarations and improve prompts

--- tarefa_2/teste.js
+++ tarefa_2/teste.js
@@ -1,37 +1,44 @@
-var qtd_homens = 0
-var qtd_mulheres = 0
-var soma_altura_homens = 0
+let qtd_homens = 0;
+let qtd_mulheres = 0;
+let soma_altura_homens = 0;
 
-for (let i = 0;i<10;i++){
-    var altura = prompt("Digite a altura: ")
-    var genero = prompt("Digite o gênero M se for mulher e H se for homem:")
+for (let i = 1; i <= 11; i++) {
 
-    if (i==1){
-        var maior_altura = altura
-        var menor_altura = altura 
+    let altura = parseFloat(prompt("Digite a altura:"));
+    let genero = prompt("Digite o gênero (M para mulher, H para homem):");
+
+    if (i === 1) {
+        var maior_altura = altura;
+        var menor_altura = altura;
     } else {
-        if (altura>maior_altura){
-            maior_altura = altura
-        }
-        
-        if (altura<menor_altura){
-            menor_altura = altura
+
+        if (altura > maior_altura) {
+            maior_altura = altura;
         }
 
-    if (genero =="H"){
-        soma_altura_homens += altura 
-        qtd_homens += 1
-    } else{
-        qtd_mulheres += 1
+        if (altura < menor_altura) {
+            menor_altura = altura;
+        }
     }
+
+    if (genero === "H") {
+        soma_altura_homens += altura;
+        qtd_homens++;
     } 
+    else if (genero === "M") {
+        qtd_mulheres++;
+    }
 }
 
-if (qtd_homens>0){
-    var media_homens = soma_altura_homens / qtd_homens
+let media_homens;
 
-} else{
-    media_homens = 0
+if (qtd_homens > 0) {
+    media_homens = soma_altura_homens / qtd_homens;
+} else {
+    media_homens = 0;
 }
 
-alert("A maior altura é (maior_altura)")
+alert("Maior altura: " + maior_altura);
+alert("Menor altura: " + menor_altura);
+alert("Média das alturas dos homens: " + media_homens);
+alert("Total de mulheres: " + qtd_mulheres);
